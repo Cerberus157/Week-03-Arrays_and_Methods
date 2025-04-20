41f708b24a74e152c42a46552c93e79cccaac49a
Cerberus157 <nyblood77@outlook.com>
2025-04-19 20:50:32 -0700

    Added sysout line

--- week3JavaQuiz.java
+++ week3JavaQuiz.java
@@ -4,7 +4,7 @@ public class week3JavaQuiz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Making this siple change to the file");
 	}
 
 }
